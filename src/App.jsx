603b737080ab7cb51c3c74da032652bc7b5b8efb
Bryan Júlio da Silva Passos <ben0066616@aluno.firjansenaisesi.com.br>
2026-06-22 21:38:16 -0300

    novo

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,153 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+function Titulo() {
+  return (
+    <h1>Maconha</h1>
+  )
+}
 
-  function ButaoSenai() {
+function Heroi() {
+  return (
+    <div className="hero">
+      <img src={heroImg} className="base" width="170" height="179" alt="" />
+    </div>
+  )
+}
 
-  const handleClick = () => {
-    alert('Você clicou no botão!')
-  }
+function Contador() {
+  const [count, setCount] = useState(0)
 
   return (
     <button
-    style={{
-      backgroundColor: 'blue',
-      color: 'white',
-      padding: '10px 20px',
-      fontWeight: 'bold',
-      border: 'none',
-
-    }}
-    onClick={handleClick}
+      type="button"
+      className="counter"
+      onClick={() => setCount((count) => count + 1)}
     >
-     Clique aqui
+      Count is {count}
     </button>
   )
 }
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+function PrimeiroComponente() {
+
+const nome = document.getElementsByName("nome");
+   const exibirAlerta = () => {
+    alert(`Você digitou: ${nome[0].value}`);
+   }
+  
   return (
-    <div>  
-      <h1>Meu app senai</h1>
-      <ButaoSenai text="Clique aqui" />
     
+  <div id="social">
+    <h1>Primeiro Componente</h1>
+    <input type="text" name="nome" placeholder="Digite seu nome" />
+    
+    <input 
+      value="Mostrar" 
+      onClick={exibirAlerta}  
+    />
 </div>
-  );
-  }
-  export default App
+  )
+}
+
+function App() {
+ 
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+         <Heroi />
+        </div>
+        <div>
+          <Titulo />
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        mijo xixi bosta merda
+      </section>
+
+<PrimeiroComponente />
+     
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Vá se ferrar</h2>
+          <p>Não seja bem vindo</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
